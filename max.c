7f30ac68d7adee68622050d0e05ea370f1776df3
KotariVinay <vinaykotari7@gmail.com>
2024-02-01 23:16:07 +0530

    day4

---
+++ max.c
@@ -0,0 +1,16 @@
+/* Write a program to find the max of two numbers */
+#include<stdio.h>
+int main()
+{
+       int num1,num2;
+       printf("Enter the value of num1 :\n");
+       printf("enter the value of num2 :\n");
+       scanf("%d \n%d",&num1,&num2);
+       if(num1>num2)
+       {
+            printf("num1 is graeter than num2\n");
+       }else
+       {
+	   printf("num2 is greater than num1\n");
+       }
+}
