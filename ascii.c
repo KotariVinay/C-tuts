5782a27d1eecafba2e666d14803f64c9375a8da1
KotariVinay <vinaykotari7@gmail.com>
2024-02-04 22:54:35 +0530

    day7

---
+++ ascii.c
@@ -0,0 +1,11 @@
+/* WAP to know the ASCII value */
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter the character :\n");
+    scanf("%c",&ch);
+    printf("The ASCII of %c = %d",ch,ch);
+    return 0;
+}
+
